532345c8001f5a876211961e5bc1a36e4a240fd7
DAH 14 <dah14@FORMATION.lan>
2020-02-03 10:28:11 +0100

    anime text

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,17 @@
-/** git fetch pour savoir si y a changement */
+var window = this;
+
+let h1 = document.querySelector('h1');
+if (h1) {
+    let text = h1.innerHTML;
+    animateText(h1, text);
+}
+
+function animateText (el, text) {
+    el.innerHTML = '';
+    for (var i = 0; i < text.length; i++) {
+        const letter = text[i]
+        setTimeout(function () {
+            el.innerHTML += letter;
+        },i*300);
+    }
+}
